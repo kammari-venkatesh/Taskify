52fbd562ec3e835179f7776a2699f6a91a91a09a
kammari-venkatesh <kammarivenkatesh57@gmail.com>
2025-09-04 15:42:14 +0530

    test

--- Backend/Server.js
+++ Backend/Server.js
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173", 
+        origin: "https://taskify-zq2i.vercel.app", 
         methods: ["GET", "POST"]
     }
 });
@@ -26,7 +26,7 @@ setSocketInstance(io);
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://taskify-zq2i.vercel.app",
     credentials: true
 }));
 app.use(cookieParser());
